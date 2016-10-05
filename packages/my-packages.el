469775fccdb7570085926d3ec013ba8423be9daf
Alex Floyd Marshall <apmarshall@soren.tech>
2016-10-05 17:03:28 -0400

    Load the packages we currently use

--- packages/my-packages.el
+++ packages/my-packages.el
@@ -11,3 +11,26 @@
 (use-package auto-compile
   :config (auto-compile-on-load-mode))
 (setq load-prefer-newer t)
+
+(require 'smex)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+
+(require 'magit)
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(with-eval-after-load 'info
+  (info-initialize)
+  (add-to-list 'Info-directory-list
+	       "~/.emacs.d/site-lisp/magit/Documentation/"))
+
+(require 'ztree)
+
+(require 'markdown-mode)
+(add-to-list 'auto-mode-alist '("\.md$" . markdown-mode))
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\.html$" "\.php$" . web-mode))
+
+(require 'projectile)
+(projectile-global-mode)
