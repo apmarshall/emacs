09d621f9d527843631bca3aa2562808bb901a3ae
Alex Floyd Marshall <apmarshall@soren.tech>
2016-10-05 16:44:09 -0400

    Some custom settings

---
+++ general/custom-settings.el
@@ -0,0 +1,15 @@
+(setq backup-directory-alist '(("." . "~/.emacs.d/backups")))
+(setq delete-old-versions -1)
+(setq version-control t)
+(setq vc-make-backup-files t)
+(setq auto-save-file-name-transforms '((".*" "~/.emacs.d/auto-save-list/" t)))
+
+(setq savehist-file "~/.emacs.d/savehist")
+(savehist-mode 1)
+(setq history-length t)
+(setq history-delete-duplicates t)
+(setq savehist-save-minibuffer-history 1)
+(setq savehist-additional-variables
+      '(kill-ring
+        search-ring
+        regexp-search-ring))
