005479fb8ae9111709a21d648833932e498aeb11
Alex Floyd Marshall <apmarshall@soren.tech>
2016-10-05 18:53:34 -0400

    Inserting time-tracking related code from Sasha Chua

---
+++ org-mode/time-tracking.el
@@ -0,0 +1,34 @@
+(use-package org
+ :load-path "~/elisp/org-mode/lisp"
+ :init
+ (progn
+  (setq org-expiry-inactive-timestamps t)
+  (setq org-clock-idle-time nil)
+  (setq org-log-done 'time)
+  (setq org-clock-continuously nil)
+  (setq org-clock-persist t)
+  (setq org-clock-in-switch-to-state "STARTED")
+  (setq org-clock-in-resume nil)
+  (setq org-show-notification-handler 'message)
+  (setq org-clock-report-include-clocking-task t))
+ :config
+  (org-clock-persistence-insinuate))
+
+(setq org-log-into-drawer "LOGBOOK")
+(setq org-clock-into-drawer 1)
+
+(setq org-habit-graph-column 80)
+(setq org-habit-show-habits-only-for-today nil)
+
+(add-hook 'org-clock-in-prepare-hook
+          'my/org-mode-ask-effort)
+
+(defun my/org-mode-ask-effort ()
+  "Ask for an effort estimate when clocking in."
+  (unless (org-entry-get (point) "Effort")
+    (let ((effort
+           (completing-read
+            "Effort: "
+            (org-entry-get-multivalued-property (point) "Effort"))))
+      (unless (equal effort "")
+        (org-set-property "Effort" effort)))))
