68848c0036a032f876b130ac688dd15a6ddc588d
Alex Floyd Marshall <apmarshall@soren.tech>
2016-10-05 16:42:55 -0400

    Bare-bones init.el

---
+++ emacs.d.symlink/init.el
@@ -0,0 +1,21 @@
+;;; package --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
+(package-initialize)
+
+(add-to-list 'load-path "~/.dotfiles/emacs")
+(add-to-list 'load-path "/usr/local/share/emacs/site-lisp")
+(setq package-enable-at-startup nil)
+
+(setq user-full-name "Alexander P. Floyd Marshall"
+      user-mail-address "apmarshall@soren.tech")
+      
+(load "~/.dotfiles/emacs/emacs.secrets" t)
+(load "~/.dotfiles/emacs/general/custom-settings.el" t)
+(load "~/.dotfiles/emacs/packages/my-packages.el" t)
+(load "~/.dotfiles/emacs/org-mode/my-org.el" t)
+
+(server-start)
