fe49b971bb559ed2d91e2c9a5c53857957b0e4a4
Alex Floyd Marshall <apmarshall@soren.tech>
2016-10-05 17:12:20 -0400

    Basic org settings

---
+++ org-mode/my-org.el
@@ -0,0 +1,11 @@
+(global-set-key "\C-cl" 'org-store-link)
+(global-set-key "\C-ca" 'org-agenda)
+(global-set-key "\C-cc" 'org-capture)
+(global-set-key "\C-cb" 'org-iswitchb)
+
+(load "~/.dotfiles/emacs/org-mode/notes.el" t)
+(load "~/.dotfiles/emacs/org-mode/tasks.el" t)
+(load "~/.dotfiles/emacs/org-mode/agenda.el" t)
+(load "~/.dotfiles/emacs/org-mode/time-tracking.el" t)
+(load "~/.dotfiles/emacs/org-mode/reporting.el" t)
+(load "~/.dotfiles/emacs/org-mode/gcal.el" t)
