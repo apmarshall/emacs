706e4b10ada276602d5ea3fa294932840cddf306
apmarshall <apmarshall@soren.tech>
2017-08-31 12:18:15 -0400

    Attempting to fix broken use-package

--- packages/my-packages.el
+++ packages/my-packages.el
@@ -1,16 +1,37 @@
-(unless (assoc-default "melpa" package-archives)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-;  (package-refresh-contents)
-)
+(setq load-prefer-newer t)
 
+(require 'package)
+(setq package-enable-at-startup nil)
+(setq package-archives
+      ;; Package archives, the usual suspects
+      '(("GNU ELPA"     . "http://elpa.gnu.org/packages/")
+        ("MELPA Stable" . "https://stable.melpa.org/packages/")
+        ("MELPA"        . "https://melpa.org/packages/"))
+      ;; Prefer MELPA Stable over GNU over MELPA.  IOW prefer MELPA's stable
+      ;; packages over everything and only fall back to GNU or MELPA if
+      ;; necessary.
+      package-archive-priorities
+
+      '(("MELPA Stable" . 10)
+        ("GNU ELPA"     . 5)
+        ("MELPA"        . 0))
+
+(package initialize)
+
+;; Bootstrap `use-package'
 (unless (package-installed-p 'use-package)
+  (package-refresh-contents)
   (package-install 'use-package))
+
+(require 'use-package)
+(require 'subr-x)
+(require 'time-date)
+
 (setq use-package-verbose t)
 (setq use-package-always-ensure t)
-(require 'use-package)
 (use-package auto-compile
   :config (auto-compile-on-load-mode))
-(setq load-prefer-newer t)
+
 
 (use-package smex
   :bind (("M-x" . smex)
